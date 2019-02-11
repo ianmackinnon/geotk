578424f66699640712ab3cf93bf2415366b9c1df
Ian Mackinnon <imackinnon@gmail.com>
2019-02-11 18:31:50 +0100

    Add depth passes to svg2gcode.

--- geotk/svg2gcode.py
+++ geotk/svg2gcode.py
@@ -38,40 +38,90 @@ def write_gcode(out, d):
 
 
 
+def sign(v):
+    if v < 0:
+        return -1
+    if v > 0:
+        return 1
+    return 0
+
+
+
 def write_paths_gcode(out, paths, conf):
     """
     Vertex numbers start from 1.
     """
 
-    out.write("G90\n")
-    out.write(f"F{format_float(conf['feedrate'])}\n")
+    assert conf["z-mill"] is not None
+    assert conf["z-safety"] is not None
 
-    out.write(f"G0 Z{format_float(conf['z-safety'])}\n")
-    for path in paths:
-        if not path:
-            continue
+    z_layer_depth = conf.get("z-layer-depth", None)
+    z_material = conf.get("z-material", None)
+    material_depth = 0
 
-        write_gcode(out, {
-            "G0": None,
-            "X": path[0][0],
-            "Y": path[0][1],
-        })
-        write_gcode(out, {
-            "G1": None,
-            "Z": conf['z-mill'],
-        })
+    if z_material is None:
+        z_material = conf["z-mill"]
 
-        for vertex in path[1:]:
+    material_depth = conf["z-mill"] - z_material
+    material_sign = sign(material_depth)
+    material_depth = abs(material_depth)
+
+    if z_layer_depth is None:
+        z_layer_depth = material_depth
+
+    write_gcode(out, {
+        "G90": None
+    })
+    write_gcode(out, {
+        "F": conf['feedrate']
+    })
+    write_gcode(out, {
+        "G0": None,
+        "Z": conf['z-safety']
+    })
+
+    layer_depth = 0
+    while True:
+        layer_depth += z_layer_depth
+
+        LOG.info("zm %s", z_material)
+        LOG.info("layer depth %s", layer_depth)
+        LOG.info("material depth %s", material_depth)
+
+        cut_depth = min(material_depth, layer_depth)
+        target_depth = z_material + material_sign * cut_depth
+
+        LOG.info("cut depth %s", cut_depth)
+        LOG.info("target depth %s", target_depth)
+
+        for path in paths:
+            if not path:
+                continue
+
+            write_gcode(out, {
+                "G0": None,
+                "X": path[0][0],
+                "Y": path[0][1],
+            })
             write_gcode(out, {
                 "G1": None,
-                "X": vertex[0],
-                "Y": vertex[1],
+                "Z": target_depth,
             })
 
-        write_gcode(out, {
-            "G1": None,
-            "Z": conf['z-safety'],
-        })
+            for vertex in path[1:]:
+                write_gcode(out, {
+                    "G1": None,
+                    "X": vertex[0],
+                    "Y": vertex[1],
+                })
+
+            write_gcode(out, {
+                "G1": None,
+                "Z": conf['z-safety'],
+            })
+
+        if not layer_depth < material_depth:
+            break
 
 
 
